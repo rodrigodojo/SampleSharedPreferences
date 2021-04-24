5f98a72e4acc61639730a1d3c1d3325f68c99000
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-24 16:28:34 -0300

    exemplo de SharedPreferences

--- app/src/main/java/com/dojo/preferenciasdousuario/MainActivity.java
+++ app/src/main/java/com/dojo/preferenciasdousuario/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -32,8 +33,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA,0);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                if(editNome.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"Preencha o nome",Toast.LENGTH_LONG).show();
+                }else{
+                    String nome = editNome.getText().toString();
+                    editor.putString("nome",nome);
+                    editor.commit();
+                    textResultado.setText("Olá, " + nome);
+                }
+
             }
         });
+
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA,0);
+
+        if(preferences.contains("nome")){
+            String nome = preferences.getString("nome","usuario não definido");
+            textResultado.setText("Olá " + nome);
+        }else{
+            textResultado.setText("Olá , usuario não definido");
+        }
 
 
     }
