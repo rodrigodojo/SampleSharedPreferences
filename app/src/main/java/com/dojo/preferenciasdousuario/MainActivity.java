e204c740b411a0f0a9397ab3fef0f2614d588168
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-24 16:17:12 -0300

    exemplo de SharedPreferences

---
+++ app/src/main/java/com/dojo/preferenciasdousuario/MainActivity.java
@@ -0,0 +1,40 @@
+package com.dojo.preferenciasdousuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSalvar;
+    private TextInputEditText editNome;
+    private TextView textResultado;
+    private static final String ARQUIVO_PREFERENCIA = "ArquivoPreferencia";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSalvar = findViewById(R.id.botaoSalvar);
+        editNome = findViewById(R.id.editNome);
+        textResultado = findViewById(R.id.textResultado);
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA,);
+            }
+        });
+
+
+    }
+}
